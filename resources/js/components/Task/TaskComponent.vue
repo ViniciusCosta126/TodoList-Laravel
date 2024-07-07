a6372d27e17488b5fb7fd979f9aac25b19927805
Vinicius Costa <54358510+ViniciusCosta126@users.noreply.github.com>
2024-07-07 02:32:44 -0300

    Criando componente de task

---
+++ resources/js/components/Task/TaskComponent.vue
@@ -0,0 +1,36 @@
+<template>
+    <div
+        :class="[
+            'flex justify-between md:justify-normal mx-auto max-w-2xl mb-3 md:w-6/12 lg:w-4/6 text-sm md:text-md lg:text-xl gap-2 md:gap-10 lg:gap-20 shadow py-2 px-1 md:px-2 lg:px-4 rounded items-center',
+            task.data.toLocaleDateString() < new Date().toLocaleDateString()
+                ? 'bg-red-light'
+                : '',
+            task.data.toLocaleDateString() === new Date().toLocaleDateString()
+                ? 'bg-yellow-light'
+                : '',
+        ]"
+    >
+        <p class="w-1/4 font-semibold cursor-pointer">
+            {{ task.titulo }}
+        </p>
+        <p class="w-1/4">
+            {{ task.data.toLocaleDateString() }}
+        </p>
+        <div class="Ações min-w-fit">
+            <i class="fas mr-1 md:mr-3 text-red fa-trash"></i>
+            <i class="fas text-yellow fa-edit"></i>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "TaskComponent",
+    props: {
+        task: {
+            type: Object,
+            required: true,
+        },
+    },
+};
+</script>
--- resources/js/pages/HomeComponent.vue
+++ resources/js/pages/HomeComponent.vue
@@ -1,15 +1,46 @@
 <template>
-    <ContainerComponent>
-        <h1>teste teste</h1>
-    </ContainerComponent>
+    <div>
+        <ContainerComponent>
+            <TaskComponent v-for="task in tasks" :key="task.id" :task="task" />
+        </ContainerComponent>
+    </div>
 </template>
 
 <script>
 import ContainerComponent from "../components/Container/ContainerComponent.vue";
+import TaskComponent from "../components/Task/TaskComponent.vue";
 export default {
     name: "HomeComponent",
+    data() {
+        return {
+            tasks: [
+                {
+                    id: 1,
+                    titulo: "teste1",
+                    descricao:
+                        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt nobis maiores provident amet? Quis, blanditiis pariatur.",
+                    data: new Date("07-07-2024"),
+                },
+                {
+                    id: 2,
+                    titulo: "teste2",
+                    descricao:
+                        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt nobis maiores provident amet? Quis, blanditiis pariatur.",
+                    data: new Date("07-06-2024"),
+                },
+                {
+                    id: 3,
+                    titulo: "teste3",
+                    descricao:
+                        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt nobis maiores provident amet? Quis, blanditiis pariatur.",
+                    data: new Date("07-17-2024"),
+                },
+            ],
+        };
+    },
     components: {
         ContainerComponent,
+        TaskComponent,
     },
 };
 </script>
